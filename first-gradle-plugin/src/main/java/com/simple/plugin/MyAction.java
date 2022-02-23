61b184a4b4100c9c9adfc746ebdc87eed104b39a
chengx <1449697757@qq.com>
2022-02-23 17:41:13 +0800

    feat：idea插件方面开发

---
+++ first-gradle-plugin/src/main/java/com/simple/plugin/MyAction.java
@@ -0,0 +1,29 @@
+package com.simple.plugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiFile;
+
+/**
+ * 项目: idea-plugins
+ * <p>
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2022-02-23 14:09
+ **/
+public class MyAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+        PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
+        String classPath = psiFile.getVirtualFile().getPath();
+
+        Messages.showMessageDialog(project, "guide-idea-plugin-create-project-by-gradle: " + classPath, "Hi IDEA Plugin", Messages.getInformationIcon());
+    }
+}
