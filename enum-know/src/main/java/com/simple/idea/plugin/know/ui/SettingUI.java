575517308c2a55f491e75ffc2ffcbafcaa783f79
“simple” <1449697757@qq.com>
2023-07-20 21:43:42 +0800

    feat：画好图

--- enum-know/src/main/java/com/simple/idea/plugin/know/ui/SettingUI.java
+++ enum-know/src/main/java/com/simple/idea/plugin/know/ui/SettingUI.java
@@ -1,7 +1,6 @@
 package com.simple.idea.plugin.know.ui;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
 import com.simple.idea.plugin.know.data.DataSetting;
 
 import javax.swing.*;
@@ -17,34 +16,38 @@ import java.io.File;
  **/
 public class SettingUI {
 
-    private JPanel mainPanel;
-    private JPanel settingPanel;
-    private JLabel urlLabel;
-    private JTextField urlTextField;
-    private JButton urlButton;
+    private JPanel main;
+    private JPanel fileSetting;
+    private JLabel selectDesc;
+    private JTextField filePathText;
+    private JButton fileSelectButton;
 
     public SettingUI(Project project) {
         // 初始化操作，默认值加入
         DataSetting.EnumKnowFileConfig enumKnowFileConfig = DataSetting.getInstance(project).getState().getEnumKnowFileConfig();
-        urlTextField.setText(enumKnowFileConfig.getEnumKnowFilePath());
+        filePathText.setText(enumKnowFileConfig.getEnumKnowFilePath());
         // 给按钮添加一个选择文件的事件
-        urlButton.addActionListener(e -> {
+        fileSelectButton.addActionListener(e -> {
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            fileChooser.showOpenDialog(settingPanel);
+            fileChooser.showOpenDialog(fileSetting);
             // 这里是上传一个文件路径
             File file = fileChooser.getSelectedFile();
             // 设置到组件属性中，之后通过这个属性拿到对应的文件流
-            urlTextField.setText(file.getPath());
+            filePathText.setText(file.getPath());
             enumKnowFileConfig.setEnumKnowFilePath(file.getPath());
         });
     }
 
     public JComponent getComponent() {
-        return mainPanel;
+        return main;
     }
 
     public JTextField getUrlTextField() {
-        return urlTextField;
+        return filePathText;
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
