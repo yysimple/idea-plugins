044a8885ff0143fcbdf9d12bc4bddc02568a672c
wuchengxing <1449697757@qq.com>
2023-07-21 17:59:13 +0800

    feat：新增图标

--- enum-know/src/main/java/com/simple/idea/plugin/know/action/EnumKnowAction.java
+++ enum-know/src/main/java/com/simple/idea/plugin/know/action/EnumKnowAction.java
@@ -6,9 +6,12 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.IconLoader;
 import com.simple.idea.plugin.know.service.EnumKnowService;
 import com.simple.idea.plugin.know.service.impl.EnumKnowServiceImpl;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
 
 /**
  * 项目: idea-plugins
@@ -27,6 +30,8 @@ public class EnumKnowAction extends AnAction {
         Project project = e.getProject();
         Editor editor = e.getData(com.intellij.openapi.actionSystem.CommonDataKeys.EDITOR);
 
+        Icon customIcon = IconLoader.getIcon("/icons/search-32.png");
+
         if (editor != null) {
             // 获取选中的文本
             SelectionModel selectionModel = editor.getSelectionModel();
@@ -36,9 +41,9 @@ public class EnumKnowAction extends AnAction {
             if (selectedText != null) {
                 System.out.println("Selected text: " + selectedText);
                 // 这里的 project：对应的是后面内容将会在 窗体中展示；title：这个是窗体的标题
-                Messages.showMessageDialog(project, enumKnowService.getMessage(selectedText), "枚举含义：", Messages.getInformationIcon());
+                Messages.showMessageDialog(project, enumKnowService.getMessage(selectedText), "枚举含义：", customIcon);
             } else {
-                Messages.showMessageDialog(project, "Please you select text", "请选择文本：：", Messages.getInformationIcon());
+                Messages.showMessageDialog(project, "Please you select text", "请选择文本：", Messages.getErrorIcon());
                 System.out.println("No text selected.");
             }
         }
